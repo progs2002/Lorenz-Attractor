4f9810cd63f060bd3735aa989679ba671ee6c442
progs2002 <pragatobhattacharjee@gmail.com>
2021-08-30 00:34:47 +0530

    scroll working

---
+++ runWindows.bat
@@ -0,0 +1,2 @@
+g++ src/*.cpp -I. -Iinclude -Iinclude/SDL2 -Llib/win -lmingw32 -lSDL2main -lSDL2 -o build/app
+.\build\app.exe
--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,24 @@ void update()
 			isRunning = false;
 			break;
 		}
+
+		if(event.type == SDL_MOUSEWHEEL)
+		{
+			if(event.wheel.y > 0)
+				theta+=10;
+			else if(event.wheel.y < 0)
+				theta-=10;
+		}
+
 	}
 	
 	scale = abs(0.1 * theta);
 
 	if(theta >= 1000.0f)
-		theta = -1000.0f;
-	
-	theta += 0.01;
+		theta = 1000.0f;
+
+	if(theta <= 1.0f)
+		theta = 1.0f;
 }
 
 void render()
@@ -66,7 +76,7 @@ int init()
 	size = getRES();
 	pen = new Renderer("Lorenz Attractor", size);
 
-	theta = -1000.0f;
+	theta = 180;
 
 	isRunning = true;
 	return 1;
@@ -83,7 +93,8 @@ int main(int argc, char* args[])
 		deltaTime = (double)((currentTick - lastTick) * 1000 / (double)SDL_GetPerformanceFrequency()); // in milliseconds
 		update();
 		render();
-		std::cout << "time taken to update and render in milliseconds -> " << deltaTime << std::endl;
+		//std::cout << "time taken to update and render in milliseconds -> " << deltaTime << std::endl;
+		std::cout << "theta = " << theta << std::endl;
 	}
 
 	delete pen;
